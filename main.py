35bd2601e190ee7dc31a06cd09a5f81e2bdbedb2
TiphM <phenitia@gmail.com>
2017-05-28 18:00:32 +0000

    Debut d'implementation camera

--- main.py
+++ main.py
@@ -1,24 +1,47 @@
 #!/usr/bin/python
 import os
 import time
+import picamera
+import io
+from PIL import Image
 
 def camera():
-    print('enfant')
-    time.sleep(3)
+    stream = io.BytesIO()
+    camera = picamera.PiCamera()
+    #camera.resolution (1024, 768)
+    camera.start_preview()
+    time.sleep(2)
+    camera.capture(stream, format='jpeg')
+    stream.seek(0)
+    image = Image.open(stream)
+    print('camera')
+    # Tourne et prend des photos, interprete le code barre
 
+    #for i, filename in enumerate(camera.capture_continuous('image{counter:02d}.jpg')):
+    #    print(filename)
+    #    time.sleep(2)
+    #    if i == 3 :
+    #        break
+    
+    #for filemane in camera.capture_continuous('img{counter:03d}.jpg'):
+    #    print ('Captured %s' % filename)
+    #    sleep(2)
+    camera.stop_preview()
+    camera.close()
 
-Municipalite = open('./configmunicipalite.txt', 'r')
-myMunicipalite = Municipalite.read()
-Municipalite.close()
+municipalite = open('./config.txt', 'r')
+myMunicipalite = municipalite.read()
+municipalite.close()
 
 if myMunicipalite == '' or myMunicipalite is None:
     print('plop')
-    # A definir
+    # Creation processus menu
 
 newCam = os.fork()
 if newCam == 0:
     camera()
 else:
+    # Creation processus menu
     os.wait()
     print('parent')
 
